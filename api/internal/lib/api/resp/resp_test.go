d35a55034bded0560f795315cfaf6cc72a6785e6
psimaf <psimaf27@gmail.com>
2025-06-28 23:32:07 +0300

    simple tests for resp

--- api/internal/lib/api/resp/resp_test.go
+++ api/internal/lib/api/resp/resp_test.go
@@ -28,24 +28,24 @@ func TestOK(t *testing.T) {
 	}
 }
 
-var ErrSomeError = errors.New("ERROR")
-
 func TestError(t *testing.T) {
 	tests := []struct {
 		name string
+		err  error
 		want Response
 	}{
 		{
 			name: "base case",
+			err:  errors.New("ERROR"),
 			want: Response{
 				Status: StatusError,
-				Error:  ErrSomeError.Error(),
+				Error:  "ERROR",
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := Error(ErrSomeError)
+			got := Error(tt.err)
 			assert.Equal(t, tt.want, got)
 		})
 	}
