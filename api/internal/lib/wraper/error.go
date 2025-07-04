7ca49aa2e2c748d3f8d16d2524a78312a84b477a
psimaf <psimaf27@gmail.com>
2025-07-04 21:20:12 +0300

    add new custom error typr for wraping

--- api/internal/lib/wraper/error.go
+++ api/internal/lib/wraper/error.go
@@ -4,40 +4,40 @@ import (
 	"fmt"
 )
 
+var (
+	_ error = &Error{}
+)
+
 type Error struct {
 	Fn  string
 	Msg string
 	Err error
 }
 
-func newError(fn string, msg string, err error) error {
+func newError(fn string, msg string, err error) *Error {
 	if isNil(err) {
 		return nil
 	}
 
-	return Error{
+	return &Error{
 		Fn:  fn,
 		Msg: msg,
 		Err: err,
 	}
 }
 
-func (e Error) Error() string {
-	return e.String()
-}
-
-func (e Error) String() string {
+func (e *Error) Error() string {
 	if isNil(e.Err) {
 		return ""
 	}
 
 	if isEmptyMsg(e.Msg) {
-		return fmt.Errorf("%s: %w", e.Fn, e.Err).Error()
+		return fmt.Sprintf("%s: %s", e.Fn, e.Err.Error())
 	}
 
-	return fmt.Errorf("%s: %s: %w", e.Fn, e.Msg, e.Err).Error()
+	return fmt.Sprintf("%s: %s: %s", e.Fn, e.Msg, e.Err.Error())
 }
 
-func (e Error) Unwrap() error {
+func (e *Error) Unwrap() error {
 	return e.Err
 }
